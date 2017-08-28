61bb578f8e4f2bb9cb948c510ca1d1d34b19fecd
Lauren Pritchett <laurenpritchett2@gmail.com>
2017-08-28 21:54:18 +0000

    rotating matrices

---
+++ rotate_2d.py
@@ -0,0 +1,29 @@
+def rotate_matrix(m):
+    N = len(m)
+    for x in range(N / 2):
+        for y in range(x, N - x - 1):
+            temp = m[x][y]
+            m[x][y] = m[y][N - x - 1]
+            m[y][N - x - 1] = m[N - x - 1][N - y - 1]
+            m[N - x - 1][N - y - 1] = m[N - 1 - y][x]
+            m[N - 1 - y][x] = temp
+
+
+def print_matrix(m):
+    N = len(m)
+    for i in range(N):
+        for j in range(N):
+            print m[i][j],
+
+        print "\n"
+    print "\n"
+
+test_m = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 16],
+          ]
+
+print_matrix(test_m)
+rotate_matrix(test_m)
+print_matrix(test_m)
